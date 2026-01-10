0296e283612276f178b44c14cf707a436a9f4275
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-01-10 01:38:43 -0800

    chore: bump version to 0.0.12

--- frontend/src/lib/version.ts
+++ frontend/src/lib/version.ts
@@ -1,5 +1,5 @@
 // Version information - automatically updated by version.js script
-export const VERSION = '0.0.11'
+export const VERSION = '0.0.12'
 export const APP_NAME = 'MedStash'
 export const DESCRIPTION = 'Self-hostable medical receipt storage system for HSA recordkeeping'
 export const AUTHOR = 'Shawn Hoffman'
