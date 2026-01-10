143359e1b779a085d5a184ebf049fbe60001357a
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-01-09 19:20:33 -0800

    refactor: enhance custom render function in test utilities
    - Integrate ErrorProvider into the custom render function for improved error handling in tests
    - Simplify import statements for better readability
    - Maintain existing functionality while enhancing test setup consistency

--- frontend/src/__tests__/helpers/testUtils.tsx
+++ frontend/src/__tests__/helpers/testUtils.tsx
@@ -1,31 +1,30 @@
-import { ReactElement } from 'react';
-import { render, RenderOptions } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { ReactElement } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import { ErrorProvider } from '../../contexts/ErrorContext'
 
 /**
  * Custom render function that includes providers
  */
-function customRender(
-  ui: ReactElement,
-  options?: Omit<RenderOptions, 'wrapper'>
-) {
-  function Wrapper({ children }: { children: React.ReactNode }) {
-    return (
-      <BrowserRouter
-        future={{
-          v7_startTransition: true,
-          v7_relativeSplatPath: true,
-        }}
-      >
-        {children}
-      </BrowserRouter>
-    );
-  }
+function customRender(ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) {
+	function Wrapper({ children }: { children: React.ReactNode }) {
+		return (
+			<ErrorProvider>
+				<BrowserRouter
+					future={{
+						v7_startTransition: true,
+						v7_relativeSplatPath: true,
+					}}
+				>
+					{children}
+				</BrowserRouter>
+			</ErrorProvider>
+		)
+	}
 
-  return render(ui, { wrapper: Wrapper, ...options });
+	return render(ui, { wrapper: Wrapper, ...options })
 }
 
 // Re-export everything
-export * from '@testing-library/react';
-export { customRender as render };
-
+export * from '@testing-library/react'
+export { customRender as render }
